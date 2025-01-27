4bef4d581c1f66ac27d26496dd036f98776b9a23
Toomad23 <107427548+Toomad23@users.noreply.github.com>
2025-01-28 00:25:30 +0300

    Update privasea_node.sh

--- privasea_node.sh
+++ privasea_node.sh
@@ -5,7 +5,8 @@ install_node() {
     echo "Обновление пакетов и установка Docker..."
     sudo apt update && sudo apt upgrade -y
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "Добавление ключа Docker и настройка репозитория..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt-get update
